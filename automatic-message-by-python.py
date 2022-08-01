dce6bd273f375f7107ec5adf837be85d983b3be4
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2022-08-01 23:56:53 +0600

    first commit

---
+++ automatic-message-by-python.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+for i in range(50):
+    time.sleep(1)
+    pyautogui.typewrite('''Dim pare Hase khay BagdaseDim pare Hase khay Bagdase''')
+    time.sleep(1)
+    pyautogui.press('enter')    
